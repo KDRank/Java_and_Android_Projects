29b66b758d054bb3f762a1a9ed0d09b9f8992a33
Kyle Rankin <krankin@gmail.com>
2024-06-27 11:59:12 -0500

    Java project submissions added with Dog example

---
+++ Java_tutorial/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /*
+     */
+    public static void main(String[] args) {
+        //Stand alone line comment
+        System.out.println("First line from app"); //Line comment
+        //System.out.println("Second line from app");
+        System.out.println(/*"Third line from app"*/ "This is different text");
+    }
+}
---
+++ OOP_Submission_Assignment/src/com/OOPSubmission/Dog.java
@@ -0,0 +1,10 @@
+package com.OOPSubmission;
+
+public class Dog {
+    public static class Main {
+        breed
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
